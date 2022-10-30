9b1c0a89c365cfdfd6b56edd8a00441fde7e802d
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-30 08:31:30 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,6 @@ mkdir build
 cd build
 
 cmake ${CMAKE_ARGS} .. \
-    -DPython_FIND_STRATEGY="LOCATION" \
-    -DPython_ROOT_DIR=${PREFIX} \
     -DCLINGO_BUILD_WITH_PYTHON=ON \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DPYCLINGO_INSTALL_DIR="${SP_DIR}" \
